399e01ee8446c30006bdfcd0f4117e338f56d9c8
Gabriele Tomè <tome.gabriele@gmail.com>
2025-05-13 15:30:56 +0200

    Add missing requirements

--- INSTALL.R
+++ INSTALL.R
@@ -43,12 +43,24 @@ if (!require("pvca", quietly = TRUE))
 if (!require("sva", quietly = TRUE))
   BiocManager::install("sva")
 
+if (!require("SummarizedExperiment", quietly = TRUE))
+  BiocManager::install("SummarizedExperiment")
+if (!require("STRINGdb", quietly = TRUE))
+  BiocManager::install("STRINGdb")
+if (!require("DEqMS", quietly = TRUE))
+  BiocManager::install("DEqMS")
+if (!require("limma", quietly = TRUE))
+  BiocManager::install("limma")
+if (!require("preprocessCore", quietly = TRUE))
+  BiocManager::install("preprocessCore")
+if (!require("sva", quietly = TRUE))
+
 if (!require("proBatch", quietly = TRUE))
   devtools::install_github("symbioticMe/proBatch", dependencies = T)
 if (!require("PhosR", quietly = TRUE))
   devtools::install_github("PYangLab/PhosR", dependencies = T)
-if (!require("ProTN", quietly = TRUE))
-  devtools::install_github("tomegabriele/ProTN", dependencies = T)
+if (!require("proTN", quietly = TRUE))
+  devtools::install_github("tomegabriele/proTN_package", dependencies = T)
 
 #Install additional packages for PhosProTN
 if (!require("shinyWidgets", quietly = TRUE))
@@ -59,7 +71,8 @@ list.of.packages <- c("BiocManager","shiny","markdown","knitr",
                       "shinydashboard","shinydashboardPlus","shinymaterial",
                       "shinyjs","shinyBS", "rmdformats","extrafont",
                       "devtools","wesanderson","svgPanZoom", "biomaRt", 
-                      "impute", "pvca","sva","proBatch","PhosR","ProTN")
+                      'SummarizedExperiment', 'STRINGdb', 'DEqMS', 'limma', 'preprocessCore',
+                      "impute", "pvca","sva","proBatch","PhosR","proTN")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) {
   message(paste0("ERROR: Some error occur durig the installation of the current package:\n\t",
